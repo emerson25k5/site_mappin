663f12d98f28507ba728f9a8460a3cb1d8076f80
emerson25k5 <110756033+emerson25k5@users.noreply.github.com>
2023-06-21 19:52:00 -0300

    Add files via upload

---
+++ cadpessoa.php
@@ -0,0 +1,34 @@
+<?php
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Recupera os valores do formulário
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $cpf = $_POST["cpf"];
+    $nascimento = $_POST["nascimento"];
+
+    // Conexão com o banco de dados
+    $conexao = mysqli_connect("Localhost", "root", "", "mappin");
+
+    // Verifica se a conexão foi estabelecida corretamente
+    if (!$conexao) {
+        die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
+    }
+
+    // Prepara a query de inserção
+    $sql = "INSERT INTO clientes (nome, email, telefone, cpf, nascimento) VALUES ('$nome','$email','$telefone','$cpf','$nascimento')";
+
+    // Executa a query
+    if (mysqli_query($conexao, $sql)) {
+        header("Location: cadastro_realizado.html");
+        exit();
+    } else {
+        echo "Erro ao inserir o registro: " . mysqli_error($conexao);
+
+    }
+
+    // Fecha a conexão com o banco de dados
+    mysqli_close($conexao);
+}
+?>
